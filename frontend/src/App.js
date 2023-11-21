6f07f57ac5b659b3ddc8762117a5ae8cbc0c0fe0
sanket <telisanket2002@gmail.com>
2023-11-22 01:18:28 +0530

    made the home page, login page, signup page, configured the slider and other filters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home.jsx';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Test from './components/Test';
+
 function App() {
+  const appStyle = {
+    margin: 0,
+    padding: 0,
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'white'
+  };
+
   return (
-    <div className="App">
-       <BrowserRouter>
-       <Routes>
-       <Route path="/" element={<Home/>}/>
-       <Route path="/login" element={<Login/>}/>
-       <Route path="/signup" element={<SignUp/>}/>
-       </Routes>
-       </BrowserRouter>
+    <div className="App" style={appStyle}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import Filter from './Filter';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-1 flex flex-col ml-20 ">
+        <h1 className="text-black-600 font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-5xl font-serif mb-8">
+          Search Properties For Rent
+        </h1>
+        <Filter />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
